725521d5a979a80670e01c36bc14394454e4e416
Roman Inflianskas <infroma@gmail.com>
2017-02-18 14:26:49 +0100

    Add command line options, switch to python 3

--- Tatoeba_anki.py
+++ Tatoeba_anki.py
@@ -1,161 +1,179 @@
-#!/usr/bin/python
+#!/usr/bin/env python
 # -*- coding: utf8 -*-
+"""A tool do download taboeba sentences and to put them into Anki
 
-import urllib2
-import urllib
-import re
-import sys
+Usage:
+    Taboeba_anki.py [--audio] [--tags] [--author] [--src-lang <lang>]... [--audio-lang <lang>]... [--target-lang <lang>] [--copy-media] [--anki-media-dir <dir>] [--all] <url>
+
+Options:
+    --audio                  Grab audio if sentences in a source language have it.
+    --tags                   Copy the tags if they exist.
+    --author                 Put author name as an extra tag.
+    --src-lang <lang>        Languages of source sentences (may be 1 or more that will appear on the question field). This should be 2-letter code ISO 639-1  https://en.wikipedia.org/wiki/List_of_ISO_639-2_codes.
+    --audio-lang <lang>      These codes are used in Tatoeba's url when you browse audio by language, e.g. cmn for Chinese in https://tatoeba.org/eng/sentences/with_audio/cmn.
+    --target-lang <lang>     Target language code (will be on the answer's fiels).
+    --copy-media             Copy media to Anki media folder.
+    --anki-media-dir <dir>   This is "collection.media" folder which is normally located in your documents folder. [Default: ~/Documents/Anki/User\ 1/collection.media/].
+    -a, --all                Combines --audio, --tags, --author, --copy-media.
+"""
+
+from future import standard_library
+standard_library.install_aliases()
+from builtins import input
+from builtins import str
+from builtins import range
+from docopt import docopt
 import csv
 import os
+import re
 import shutil
+import sys
+import urllib.request, urllib.parse, urllib.error
+import requests
+import logging
+logging.basicConfig(level=logging.INFO)
 
-#######################################
-# basic variables you can change
-#######################################
+args = docopt(__doc__)
 
-UrlListOfSentences = 'https://tatoeba.org/eng/sentences_lists/show/4022/none/' # basic url with the list of the sentences (if there are many pages they will be processed page by page)
-getAudio = True # True if we grab audio if sentences in a source language have it
-getTags = True # True if we copy the tags if they exist
-getAutor = True # True if we want to know who is the author (will appear as an extra tag)
-srclang = ["en","es","ar"] # languages of source sentences (may be 1 or more that will appear on the question field). This should be 2-letter code ISO 639-1  https://en.wikipedia.org/wiki/List_of_ISO_639-2_codes
-audio3letterslangcodes = ["eng","spa","ara"] # these codes are used in Tatoeba's url when you browse audio by language, e.g. cmn for Chinese in https://tatoeba.org/eng/sentences/with_audio/cmn
-targetlang = "ru" # target language code (will be on the answer's fiels)
-copymediafilestoankifolder = True # if true you should manually set your anki media folder
-ankimediafolder = "C:\\Documents\\Anki\\1-й пользователь\\collection.media"  # this is "collection.media" folder which is normally located in your documents folder (in Windows)
 
-########################################
-# Here the main code begins
-########################################
+UrlListOfSentences = args['<url>']
+getAudio = args['--audio']
+getTags = args['--tags']
+getAuthor = args['--author']
+srclang = args['--src-lang']
+audio3letterslangcodes = args['--audio-lang']
+targetlang = args['--target-lang']
+copymediafilestoankifolder = args['--copy-media']
+ankimediafolder = args['--anki-media-dir']
+if args['--all']:
+    getAudio = True
+    getTags = True
+    getAuthor = True
+    copymediafilestoankifolder = True
+
 
 if os.path.exists('foranki'):
-    key = raw_input("'foranki' folder already exists. Press Enter to clean it or close this window")
+    key = input("'foranki' folder already exists. Press Enter to clean it or close this window")
     if not key:
         shutil.rmtree('foranki')
 
 try:
     os.mkdir('foranki')
 except:
-    print "The script couldn't create a temporary workdir foranki."
+    logging.error("The script couldn't create a temporary workdir foranki.")
     sys.exit(1)
-    
-cfile = open("foranki/exampledeck.csv", "wb")
+
+cfile = open("foranki/exampledeck.csv", "w")
 
 def procstring(string):
     res = string
-    res=res.replace("&#039;","'")
-    res=res.replace("&quot;",'"')
+    res = res.replace("&#039;","'")
+    res = res.replace("&quot;",'"')
     return res
 
 # process the link, open it and grab all we need
 def proclink(num):
     taglist = []
     url = 'https://tatoeba.org/eng/sentences/show/' + num
-    
+
     curaudio = ''
-    resp = urllib2.urlopen(url)
-    if resp.getcode() != 200:
-        print "Error response for search"
+    resp = requests.get(url)
+    if resp.status_code != 200:
+        logging.error("Error response for search")
         sys.exit(1)
-    html = resp.read()
     if getTags:
-        tagname=re.findall('class="tagName".+?\>(.+?)\<',html,re.DOTALL)
+        tagname = re.findall('class="tagName".+?\>(.+?)\<', resp.text, re.DOTALL)
         for i in tagname:
-            taglist.append(i.strip().replace(" ","_"))
-    if getAutor:
-        authorname=re.findall('title="belongs\sto\s(.+?)"',html)
+            taglist.append(i.strip().replace(" ", "_"))
+    if getAuthor:
+        authorname = re.findall('title="belongs\sto\s(.+?)"', resp.text)
         if len(authorname) > 0:
             taglist.append('by_' + authorname[0])
         else:
             taglist.append('orphan_sentence')
-    srcsentence=''
+    srcsentence = ''
     mainlang = ''
     for i,item in enumerate(srclang):
-        srcsentence = re.findall('mainSentence.+?<div lang="' + item + '" dir="\w{3}" ng-non-bindable="" class="text correctnessZero">(.+?)<\/div><\/div><\/div>',html)
+        srcsentence = re.findall('mainSentence.+?<div lang="' + item + '" dir="\w{3}" ng-non-bindable="" class="text correctnessZero">(.+?)<\/div><\/div><\/div>', resp.text)
 
         if len(srcsentence) > 0:
             srcsentence = srcsentence[0]
-            mainlang=audio3letterslangcodes[i]
+            mainlang = audio3letterslangcodes[i]
             break
         else:
-            srcsentence=''
+            srcsentence = ''
             continue
-    if srcsentence=='':
-        print "Error while trying to get the source sentence"
+    if srcsentence == '':
+        logging.error("Error while trying to get the source sentence")
         return
 
     audiourl = 'https://audio.tatoeba.org/sentences/' + mainlang + '/' + num + '.mp3'
     if getAudio:
-        laudio = re.findall("https\:\/\/audio\.tatoeba\.org\/sentences\/(\w{3})\/" + num + ".mp3",html)
+        laudio = re.findall("https\:\/\/audio\.tatoeba\.org\/sentences\/(\w{3})\/" + num + ".mp3", resp.text)
         if laudio != []:
             # grab audio
-            urllib.urlretrieve(audiourl,"foranki/" + num + ".mp3")
+            urllib.request.urlretrieve(audiourl,"foranki/" + num + ".mp3")
             curaudio = '[sound:' + num + '.mp3]'
 
-    html = html.replace('ng-non-bindable=""','')
-    targetsentence = re.findall('directTranslation".+?<div lang="' + targetlang + '" dir="\w{3}"\s+class="text correctnessZero">(.+?)<\/div>',html)
+    targetsentence = re.findall('directTranslation".+?<div lang="' + targetlang + '" dir="\w{3}"\s+class="text correctnessZero">(.+?)<\/div>', resp.text.replace('ng-non-bindable=""',''))
     if len(targetsentence) > 0:
-        targetsentence=targetsentence[0]
+        targetsentence = targetsentence[0]
     else:
-        targetsentence=''
+        targetsentence = ''
 
     csv_writer = csv.writer(cfile, delimiter='\t', lineterminator='\n')
-    print " ".join([srcsentence + curaudio, targetsentence, " ".join(taglist)])
+    logging.info(" ".join([srcsentence + curaudio, targetsentence, " ".join(taglist)]))
     csv_writer.writerow([procstring(srcsentence) + curaudio, procstring(targetsentence), " ".join(taglist)])
-   
+
 
 def mainproc():
     # 1. get the list of sentences from the first page
     global UrlListOfSentences
-    UrlListOfSentences=UrlListOfSentences.replace('/page:1','').rstrip("/")
-    resp = urllib2.urlopen(UrlListOfSentences + '/page:1')
-    if resp.getcode() != 200:
-        print "Failed to open " + UrlListOfSentences
+    UrlListOfSentences = UrlListOfSentences.replace('/page:1','').rstrip("/")
+    resp = requests.get(UrlListOfSentences + '/page:1')
+    if resp.status_code != 200:
+        logging.error("Failed to open " + UrlListOfSentences)
         sys.exit(1)
-    html = resp.read()
     # how many pages there are in this list
-    pagescount = re.findall('/page\:(\d+?)\D',html)
+    pagescount = re.findall('/page\:(\d+?)\D', resp.text)
     if pagescount != []:
         pagescount = max([int(x) for x in pagescount])
     else:
         pagescount = 0 # there is no pagination
 
-    print html
+    logging.debug(resp.text)
 
-    links = re.findall('class="md-icon-button" href="/\w\w\w/sentences/show/(.+?)\"\>',html,re.DOTALL)
-    sentences = re.findall('<div class="text" flex>\s+(.+?)</div>',html)
-
-    resp.close()
+    links = re.findall('class="md-icon-button" href="/\w\w\w/sentences/show/(.+?)\"\>', resp.text, re.DOTALL)
 
     for i in range(len(links)):
-        # print links[i] + " " + sentences[i]
         proclink(links[i])
 
     prCnt = 1 # this is a progress counter (not really necessary but kind of convenient feature)
 
-    for pagescounter in xrange(2,pagescount + 1):
+    for pagescounter in range(2,pagescount + 1):
         urlloop = UrlListOfSentences.rstrip("/") + "/page:" + str(pagescounter)
-        resp = urllib2.urlopen(urlloop)
-        if resp.getcode() != 200:
-            print "Failed to open " + urlloop
+        resp = requests.get(urlloop)
+        if resp.status_code != 200:
+            logging.error("Failed to open " + urlloop)
             sys.exit(1)
-        html = resp.read()
-        # print html
-        links = re.findall("<div data-sentence-id=\"(.+?)\"\sclass",html,re.DOTALL)
-        sentences = re.findall('<div lang="\w{2,}" dir=\".+\" class=\"text correctnessZero\"\>(.+?)</div>',html)
-        resp.close()
+        links = re.findall('class="md-icon-button" href="/\w\w\w/sentences/show/(.+?)\"\>', resp.text, re.DOTALL)
         for i in range(len(links)):
             proclink(links[i])
         prCnt += 1
         curPrcnt = (100.0*prCnt) / pagescount
-        os.system('title ' + str(round(curPrcnt,3)) + '% completed')
-        
+        current_percent_completed = str(round(curPrcnt, 3)) + '% completed'
+        if shutil.which('title'):
+            os.system('title ' + current_percent_completed)
+        else:
+            logging.info(current_percent_completed)
+
     # copy media files to anki media folder
     for root, dirs, files in os.walk('foranki'):
         for f in files:
             filename = os.path.join(root, f)
             if filename.endswith('.mp3'):
                 if copymediafilestoankifolder:
-                    shutil.copy2(filename.decode('utf8'),ankimediafolder.decode('utf8'))
-    
+                    shutil.copy2(filename, ankimediafolder)
+
 mainproc()
 cfile.close()
