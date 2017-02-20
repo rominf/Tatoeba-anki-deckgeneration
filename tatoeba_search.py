ae12de60d241e56b57a8f1d630fe3721f7d3e98b
Roman Inflianskas <infroma@gmail.com>
2017-02-20 12:17:34 +0100

    Stability, speed

--- tatoeba_search.py
+++ tatoeba_search.py
@@ -22,17 +22,17 @@ from splinter import Browser
 from time import sleep
 import selenium
 import logging
-logging.basicConfig(level=logging.INFO, format='%(message)s')
-
-
-url = 'https://tatoeba.org/eng'
+logging.basicConfig(level=logging.DEBUG, format='%(message)s')
 
 
 def wait(check_function):
     start_time = datetime.now()
     while (not check_function()) and ((datetime.now() - start_time).seconds < 8):
-        pass
-    if not check_function():
+        sleep(1)
+    result = check_function()
+    if result:
+        return result
+    else:
         raise Exception('Failed to load the page')
 
 
@@ -44,9 +44,22 @@ def login(browser, username, password):
     wait(lambda: browser.is_element_visible_by_xpath('//*[@id="profile"]/a'))
 
 
+def nothing_was_found(browser):
+    return 'No results found for: ' in browser.html
+
+
+def results_page_loaded(browser):
+    current_page_css = '#main_content > div > div:nth-child(2) > span > span.current.pageNumber'
+    return (nothing_was_found(browser)
+            or (browser.is_element_visible_by_css(current_page_css) and
+                browser.find_by_css(current_page_css)[0].text)
+            or browser.is_element_visible_by_css('.addToList'))
+
+
 def search(browser, phrase, language_from, language_to, with_audio):
     browser.click_link_by_text('Advanced search')
     wait(lambda: browser.is_element_present_by_xpath('//*[@id="main_content"]/div/h2[text() = "Advanced search"]'))
+    sleep(browser.wait_time)
     browser.find_by_id('AdvancedSearchQuery').fill(phrase)
     browser.find_by_xpath(
         f'//*[@id="AdvancedSearchFrom"]//*[@value="{language_from}"]')[0].click()
@@ -56,46 +69,36 @@ def search(browser, phrase, language_from, language_to, with_audio):
         browser.find_by_id('AdvancedSearchHasAudio')[0].select_by_text('Yes')
     [search for search in browser.find_by_text('Advanced search')
      if search.tag_name == 'button'][0].click()
+    wait(lambda: results_page_loaded(browser))
 
 
 def add_all_sentences_from_the_page(browser, list_name):
-    def no_results():
-        return 'No results found for: ' in browser.html
-
-    wait(lambda: browser.is_element_visible_by_css('.addToList') or no_results())
-    if no_results():
+    if nothing_was_found(browser):
         return
+
     for show_add_to_list_button_button in browser.find_by_css('.addToList'):
         show_add_to_list_button_button.click()
-
-    list_name_xpath = f'//*[contains(@class, "listOfLists")]//*[text()="{list_name}"]'
-    wait(lambda: browser.is_element_visible_by_xpath(list_name_xpath) or browser.is_element_visible_by_css('.validateButton'))
-    for list_name_combobox in browser.find_by_xpath(list_name_xpath):
-        if list_name_combobox.visible:
-            list_name_combobox.click()
-    for add_to_list_button in browser.find_by_css('.validateButton'):
-        if add_to_list_button.visible:
-            add_to_list_button.click()
+        list_name_xpath = f'//*[contains(@class, "listOfLists")]//*[text()="{list_name}"]'
+        [list_name_combobox
+         for list_name_combobox in browser.find_by_xpath(list_name_xpath)
+         if list_name_combobox.visible][0].click()
+        [add_to_list_button
+         for add_to_list_button in browser.find_by_css('.validateButton')
+         if add_to_list_button.visible][0].click()
+        show_add_to_list_button_button.click()
 
 
 def add_all_sentences(browser, first_page, last_page, list_name):
-    loaded_page, page = None, 1
+    loaded_page, page = wait(lambda: results_page_loaded(browser)), 1
     if page >= first_page:
         logging.info(f'Page {page}')
         add_all_sentences_from_the_page(browser=browser, list_name=list_name)
     next_button = browser.find_by_css('.next')
-    while next_button and (page < last_page):
+    while (next_button) and (last_page is not None) and (page < last_page):
         page += 1
         next_button[0].click()
-        current_page_css = '#main_content > div > div:nth-child(2) > span > span.current.pageNumber'
         while loaded_page != str(page):
-            page_loaded = False
-            while not page_loaded:
-                try:
-                    page_loaded = browser.is_element_visible_by_css(current_page_css)
-                    loaded_page = browser.find_by_css(current_page_css)[0].text
-                except:
-                    pass
+            loaded_page = wait(lambda: results_page_loaded(browser))
         next_button = browser.find_by_css('.next')
         if page >= first_page:
             logging.info(f'Page {page}')
@@ -104,7 +107,11 @@ def add_all_sentences(browser, first_page, last_page, list_name):
 
 def main(args):
     with Browser() as browser:
-        browser.visit(url)
+        browser.visit('https://tatoeba.org/eng')
+        try:
+            last_page = int(args['--last-page'])
+        except TypeError:
+            last_page = None
         login(
             browser=browser,
             username=args['--username'],
@@ -112,9 +119,8 @@ def main(args):
         for phrase in args['<phrase>']:
             added = False
             while not added:
+                logging.info(f'{phrase}:')
                 try:
-                    sleep(browser.wait_time)
-                    logging.info(f'{phrase}:')
                     search(
                         browser=browser,
                         phrase=phrase,
@@ -124,7 +130,7 @@ def main(args):
                     add_all_sentences(
                         browser=browser,
                         first_page=int(args['--first-page']),
-                        last_page=int(args['--last-page']),
+                        last_page=last_page,
                         list_name=args['--list'])
                     added = True
                 except Exception as e:
